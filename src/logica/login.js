607cb77fe6caffb80b8f587ef71f99b2b6d2d517
Jorge <jgragia@epsg.upv.es>
2022-04-09 17:18:26 +0200

    H1 - T3 - Login

---
+++ src/logica/login.js
@@ -0,0 +1,78 @@
+// Nombre fichero: login.js
+// Fecha: WIP
+// Autor: Jorge Grau Giannakakis
+// Descripción: Gestiona la logica del registro e inicio de sesión a la pagina web
+
+import{ firebaseAuth } from  '../firebase.js';
+
+
+/**
+ * Efectua el registro por correo y contraseña
+ * -> registroCorreoYContrasena -> 
+ */
+function registroCorreoYContrasena(){
+
+    var correo = document.getElementById("campo_correo").value;
+    var contrasena = document.getElementById("campo_contrasena").value;
+    var contrasenaConfirmar = document.getElementById("campo_contrasena_confirmar").value;
+    
+    if(correo != "" && contrasena != "" && contrasenaConfirmar != "" && contrasena == contrasenaConfirmar){
+      firebaseAuth.createUserWithEmailAndPassword(firebaseAuth.getAuth(),correo, contrasena).catch(function(error) {
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        
+            window.alert("Error : " + errorMessage + " Codigo de error: " + errorCode);
+        
+          });
+    }
+
+}
+
+/**
+ * Permite inicar sesión usando el correo y la contraseña
+ * -> iniciarSesion -> 
+ */
+function iniciarSesion(){
+  var correo = document.getElementById("campo_correo").value;
+  var contrasena = document.getElementById("campo_contrasena").value;
+  localStorage.setItem("SesionIniciada", 1);
+  firebaseAuth.signInWithEmailAndPassword(firebaseAuth.getAuth(), correo, contrasena).catch(function(error) {
+
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      window.alert("Error : " + errorMessage + " Codigo de error: " + errorCode);
+  });
+
+  setTimeout(function(){
+    location.reload();
+}, 500);
+  
+}
+  
+/**
+ * Cerrar sesión
+ * -> cerrarSesion -> 
+ */
+function cerrarSesion(){
+  firebaseAuth.signOut(firebaseAuth.getAuth());
+  localStorage.setItem("SesionIniciada", 0);
+  location.href = '../index.html'; 
+}
+
+/**
+ * El google login funciona como inicio de sesión y registro
+ * -> GoogleLogin-> 
+ */
+function GoogleLogin(){
+
+  let provider = new firebaseAuth.GoogleAuthProvider();
+
+  firebaseAuth.signInWithPopup(firebaseAuth.getAuth(), provider).then(res=>{
+  }).catch(e=>{
+    console.log(e)
+  })
+}
+
+export { GoogleLogin, cerrarSesion, iniciarSesion, registroCorreoYContrasena };
---
+++ src/ux/controladores/controladorLogin.js
@@ -0,0 +1,37 @@
+// Nombre fichero: controladorLogin.js
+// Fecha: WIP
+// Autor: Jorge Grau Giannakakis
+// Descripción: El controlador de la pagina del login, llama a las funciones de la logica que necesite
+
+import { firebaseAuth } from '../../firebase.js';
+import { GoogleLogin, iniciarSesion } from '../../logica/login.js'
+
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+
+  // Cuando cargue la pagina se ejecutan las funciones
+
+  firebaseAuth.onAuthStateChanged(firebaseAuth.getAuth(), function(user) {
+    // Si el usuario esta registrado
+    var sesion = localStorage.getItem("SesionIniciada");
+    console.log(sesion);
+    if (user) {
+      if(sesion == 1){
+        console.log("Hola2")
+        var uid = user.uid;
+        console.log(uid + "");
+        localStorage.setItem("UID", uid);
+  
+        location.href = '../ux/storage.html';
+      }
+      }
+    });
+  addButtonEventListeners();
+});
+
+function addButtonEventListeners() {
+  document.getElementById("btn-iniciar-sesion").addEventListener("click", () => {
+    iniciarSesion()});
+  document.getElementById("btn-iniciar-sesion-google").addEventListener("click", () => {
+    GoogleLogin()});
+}
