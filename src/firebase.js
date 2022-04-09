185f1e0c5b36e4bc5228c5f18a0906d2b098ec3e
Jorge <jgragia@epsg.upv.es>
2022-04-09 17:18:49 +0200

    H011 - T3 - Diseños base de datos

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js'
 import { getFirestore, collection, query, where, getDocs, doc } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js'
 import * as firebaseAuth from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"
+import { getStorage, ref, uploadBytes, getDownloadURL  } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 
 // Configuración de firebase
 var firebaseConfig = {
     apiKey: "AIzaSyDPjhcoNszTjWKoe_rubmmYZE--GX9tTL0",
     authDomain: "hamponator-web.firebaseapp.com",
+    databaseURL: "https://hamponator-web-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "hamponator-web",
     storageBucket: "hamponator-web.appspot.com",
     messagingSenderId: "910773774186",
@@ -19,8 +21,9 @@ var firebaseConfig = {
 };
 
 // Inicializar app de Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Inicializar y exportar instancia de Firestore
-const db = getFirestore();
-export { db, collection, query, where, getDocs, firebaseAuth };
+const db = getFirestore(app);
+const dbStorage = getStorage(app);
+export { db, dbStorage, collection, query, where, getDocs, firebaseAuth, getStorage, ref, uploadBytes, getDownloadURL };
