1dd0764cf3fd551b369bb84f8a9cab249e1a7806
Adrian <admallla@epsg.upv.es>
2022-05-11 15:18:24 +0200

    H7 - T2 - Crear rutas con checkpoints en web

---
+++ src/my_first_rosweb/Checkpoints.js
@@ -0,0 +1,7 @@
+class Checkpoints {
+
+    constructor(lista_checkpoints) {
+        this.lista_checkpoints = lista_checkpoints
+    }
+
+}
---
+++ src/my_first_rosweb/Mapa.js
@@ -0,0 +1,5 @@
+class Mapa{
+    constructor(canvas,limites,centro){
+
+    }
+}
